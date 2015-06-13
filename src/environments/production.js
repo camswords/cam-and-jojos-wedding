466ec9660c1dd9c9b3973a0eaccd1ad9be5abf63
Cam Swords <cam.swords@gmail.com>
2015-06-13 18:03:22 +1000

    use the heroku url until we have setup the domain name

--- src/environments/production.js
+++ src/environments/production.js
@@ -2,5 +2,5 @@
 module.exports = {
     environment: function() { return "production"; },
     port: function() { return Number(process.env.PORT); },
-    url: function() { return 'http://www.camandjojoswedding.com/'; }
+    url: function() { return 'http://camnjojoswedding.herokuapp.com/'; }
 };
