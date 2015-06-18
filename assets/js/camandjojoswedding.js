3718975c54a5eb820b18e50e773711e625437f73
Cam Swords <cam.swords@gmail.com>
2015-06-18 20:08:48 +1000

    added modules as a concept so that we can build the rsvp page

--- assets/js/camandjojoswedding.js
+++ assets/js/camandjojoswedding.js
@@ -1,10 +1,15 @@
 
 var $ = require('jquery');
 var fullPage = require('fullpage.js');
+var modules = require('./modules/modules');
 
 $(document).ready(function() {
+    modules.execute($('.dynamic-module'));
+    
     $('#fullpage').fullpage({
         anchors: ['first-page', 'second-page'],
         menu: '#menu'
     });
+
+    
 });
---
+++ assets/js/modules/modules.js
@@ -0,0 +1,27 @@
+
+var _ = require('lodash');
+var $ = require('jquery');
+
+var registeredModules = {
+    'submit-rsvp': require('./submit-rsvp')
+};
+
+module.exports = function(modules) {
+    _.each(modules, function(module) {
+        var data = $(module).data();
+        data.module = $(module);
+        data.moduleDomElement = module;
+
+        if (!data || !data.name) {
+            console.log('modules: failed to find module-name on element', module);
+            return;
+        }
+
+        if (!registeredModules[data.name]) {
+            console.log('modules: failed to find module with name', data.name, ', will not execute module.');
+            return;
+        }
+
+        registeredModules[data.name](data);
+    });
+};
