776434a985e3dbe52dfd3930dbac54693623647f
Cam Swords <cam.swords@gmail.com>
2015-06-28 17:19:45 +1000

    removed rsvp as a choice for the moment

--- assets/js/camandjojoswedding.js
+++ assets/js/camandjojoswedding.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     modules.execute($('.dynamic-module'));
     
     $('#fullpage').fullpage({
-        anchors: ['welcome-section', 'rsvp-section', 'cant-come-section', 'agenda-section', 'kids-section', 'gifts-section', 'accommodation-section', 'aboutus-section'],
+        anchors: ['welcome-section', 'cant-come-section', 'agenda-section', 'kids-section', 'gifts-section', 'accommodation-section', 'aboutus-section'],
         menu: '#menu'
     });
 
