12c117985caa7e0feae647855f613939bb5243ef
Cam Swords <cam.swords@gmail.com>
2015-07-04 17:10:52 +1000

    updated the prod config file

--- src/environments/production.js
+++ src/environments/production.js
@@ -2,7 +2,7 @@
 module.exports = {
     environment: function() { return "production"; },
     port: function() { return Number(process.env.PORT); },
-    url: function() { return 'http://camnjojoswedding.herokuapp.com/'; },
+    url: function() { return 'http://www.camandjojoswedding.com/'; },
     mongodb: {
         url: function() { return process.env.MONGODB_URL; }
     },
