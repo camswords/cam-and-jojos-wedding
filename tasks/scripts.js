f795d9fbc9878f0b8236991242dcb615cb5e9d53
Cam Swords <cam.swords@gmail.com>
2015-08-01 14:01:14 +1000

    upgraded files to see if it removes any issues with scrolling

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,36 +1,28 @@
 var gulp = require('gulp');
 var gulpif = require('gulp-cond');
 var streamqueue = require('streamqueue');
-var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var plumber = require('gulp-plumber');
 var extend = require('extend');
 var config = require('../src/config');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 
-module.exports = function(overrides) {
+module.exports = function() {
     return function() {
-        var options = extend({}, { continueOnError: false }, overrides);
 
-        var vendorScripts = ['./vendor/semanticui/dist/semantic.min.js'];
+        return browserify(['./vendor/semanticui/dist/semantic.min.js', './assets/js/camandjojoswedding.js'])
+               .bundle()
+               .pipe(source('bundles.js'))
+               .pipe(buffer())
+               .pipe(uglify())
+               .pipe(gulp.dest('public/'));
 
-        var vendor = gulp.src(vendorScripts)
-            .pipe(concat('vendor-scripts.js'));
+//            .pipe(gulpif(config.minifyJavascript(), uglify()))
+//            .pipe(concat('minified-scripts.js'));
 
-        var application = gulp.src(['./assets/js/camandjojoswedding.js'])
-            .pipe(gulpif(options.continueOnError, plumber({
-                handleError: function(error) {
-                    console.log('error:', error);
-                    this.emit('end');
-                }
-            })))
-            .pipe(browserify({ noParse: ['jquery'] }))
-            .pipe(gulpif(config.minifyJavascript(), uglify()))
-            .pipe(concat('minified-scripts.js'));
-
-
-        return streamqueue({ objectMode: true }, application, vendor)
-            .pipe(concat('scripts.js'))
-            .pipe(gulp.dest('public/'));
+//        return streamqueue({ objectMode: true }, application, vendor)
+//            .pipe(concat('scripts.js'))
     };
 };
